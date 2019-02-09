ab20992b1bea392799574ab4689ef07a0603a2bb
aleksey leichenko <ol.leichenko@gmail.com>
2019-02-09 15:49:47 +0200

    test for git

--- src/index.js
+++ src/index.js
@@ -2,5 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import {Provider} from 'react-redux';
+import store from "./store";
+import {BrowserRouter} from 'react-browser-dom';
+import {createStore} from 'redux';
+import rootReducer from './reducers/index'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// ReactDOM.render(<App />, document.getElementById('root'));
+
+
+ReactDOM.render(
+    <Provider store ={store}>
+        <BrowserRouter><App /></BrowserRouter>
+    </Provider>
+    , document.getElementById('root')
+);
